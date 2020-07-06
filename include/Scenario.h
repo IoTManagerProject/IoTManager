bc8e3fce55f74eeac769a4521413ebfa4ff28e5a
Yuri Trikoz <ytrikoz@gmail.com>
2020-07-06 18:36:15 +0300

    Snenario parse on load #2

--- include/Scenario.h
+++ include/Scenario.h
@@ -4,6 +4,60 @@
 
 #include "Base/StringQueue.h"
 
+enum EquationSign {
+    OP_EQUAL,
+    OP_NOT_EQUAL,
+    OP_LESS,
+    OP_LESS_OR_EQAL,
+    OP_GREATER,
+    OP_GREATER_OR_EQAL
+};
+
+class ParamItem {
+   public:
+    virtual const String value();
+};
+
+class LiveParam : public ParamItem {
+   public:
+    LiveParam(const String& name);
+    const String value() override;
+
+   private:
+    String _name;
+};
+
+class StaticParam : public ParamItem {
+   public:
+    StaticParam(const String& value);
+    const String value() override;
+
+   private:
+    String _value;
+};
+
+class ScenarioItem {
+   public:
+    ScenarioItem(const String& str);
+    bool isValid();
+    bool isEnabled();
+    bool enable(bool value);
+    void run(const String& value);
+
+   private:
+    bool equation(const String& object, const String& value);
+    bool parseCondition(const String& str);
+    bool parseSign(const String& str, EquationSign& sign);
+
+   private:
+    bool _valid;
+    bool _enabled;
+    String _commands;
+    String _obj;
+    EquationSign _sign;
+    ParamItem* _param;
+};
+
 namespace Scenario {
 void reinit();
 
