9fd88ffae1c27bac2db3eb59373b6da8c086287b
Dmitry Borisenko <49808844+DmitryBorisenko33@users.noreply.github.com>
2020-12-13 01:06:08 +0300

    4mb build version

--- src/items/SensorAnalogClass.cpp
+++ src/items/SensorAnalogClass.cpp
@@ -1,21 +1,21 @@
-#include "BufferExecute.h"
-#include "items/SensorAnalogClass.h"
-#ifdef ANALOG_ENABLED
-//==============================================Модуль аналогового сенсора===========================================================================================
-//===================================================================================================================================================================
-SensorAnalogClass mySensorAnalog;
-void analogAdc() {
-    mySensorAnalog.update();
-    String key = mySensorAnalog.gkey();
-    sCmd.addCommand(key.c_str(), analogReading);
-    sensorReadingMap10sec += key + ",";
-    mySensorAnalog.SensorAnalogInit();
-    mySensorAnalog.clear();
-}
-
-void analogReading() {
-    String key = sCmd.order();
-    String pin = jsonReadStr(configOptionJson, key + "_pin");
-    mySensorAnalog.SensorAnalogRead(key, pin);
-}
-#endif
+//#include "BufferExecute.h"
+//#include "items/SensorAnalogClass.h"
+//#ifdef ANALOG_ENABLED
+////==============================================Модуль аналогового сенсора===========================================================================================
+////===================================================================================================================================================================
+//SensorAnalogClass mySensorAnalog;
+//void analogAdc() {
+//    mySensorAnalog.update();
+//    String key = mySensorAnalog.gkey();
+//    sCmd.addCommand(key.c_str(), analogReading);
+//    sensorReadingMap10sec += key + ",";
+//    mySensorAnalog.SensorAnalogInit();
+//    mySensorAnalog.clear();
+//}
+//
+//void analogReading() {
+//    String key = sCmd.order();
+//    String pin = jsonReadStr(configOptionJson, key + "_pin");
+//    mySensorAnalog.SensorAnalogRead(key, pin);
+//}
+//#endif
