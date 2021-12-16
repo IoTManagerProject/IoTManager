15b55a1c83f7f2088b1236d9baaf8bc50331f054
Dmitry Borisenko <49808844+DmitryBorisenko33@users.noreply.github.com>
2021-12-16 21:41:00 +0100

    122

--- src/WebSocket.cpp
+++ src/WebSocket.cpp
@@ -41,23 +41,3 @@ void wsPublishData(String topic, String data) {
     data = "[" + topic + "]" + data;
     ws.textAll(data);
 }
-
-// void sendDataWs(AsyncWebSocketClient* client) {
-//     // DynamicJsonBuffer jsonBuffer;
-//     // JsonObject& root = jsonBuffer.createObject();
-//     // root["a"] = "abc";
-//     // root["b"] = "abcd";
-//     // root["c"] = "abcde";
-//     // root["d"] = "abcdef";
-//     // root["e"] = "abcdefg";
-//     // size_t len = root.measureLength();
-//     AsyncWebSocketMessageBuffer* buffer = ws.makeBuffer(len);  //  creates a buffer (len + 1) for you.
-//     if (buffer) {
-//         root.printTo((char*)buffer->get(), len + 1);
-//         if (client) {
-//             client->text(buffer);
-//         } else {
-//             ws.textAll(buffer);
-//         }
-//     }
-// }
