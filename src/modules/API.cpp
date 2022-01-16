b79233d6fb7195799414a183300df1370e051a73
biver <batata@mail.ru>
2022-01-16 13:13:01 +0500

    Подключаем DS18B20

--- src/modules/API.cpp
+++ src/modules/API.cpp
@@ -3,6 +3,7 @@
 //============================================================================================
 //здесь скопируйте строку и вставьте ниже, заменив имя AnalogAdc на название вашего сенсора
 void* getAPI_AnalogAdc(String subtype, String params);
+void* getAPI_ds18b20(String subtype, String params);
 //============================================================================================
 
 void* getAPI(String subtype, String params) {
@@ -12,5 +13,7 @@ void* getAPI(String subtype, String params) {
     if ((tmpAPI = getAPI_AnalogAdc(subtype, params)) != nullptr) return tmpAPI;
     //================================================================================================================
 
+    if ((tmpAPI = getAPI_ds18b20(subtype, params)) != nullptr) return tmpAPI;
+     
     return nullptr;
 }
