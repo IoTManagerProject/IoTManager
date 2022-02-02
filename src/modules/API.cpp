dca2a518436db1febf408f3168b076d21719e4fb
biver <batata@mail.ru>
2022-02-02 11:07:27 +0300

    Портируем сенсор Bmp280

--- src/modules/API.cpp
+++ src/modules/API.cpp
@@ -6,6 +6,7 @@ void* getAPI_AnalogAdc(String subtype, String params);
 void* getAPI_Ds18b20(String subtype, String params);
 void* getAPI_Sht20(String subtype, String params);
 void* getAPI_Dht1122(String subtype, String params);
+void* getAPI_Bmp280(String subtype, String params);
 //============================================================================================
 
 void* getAPI(String subtype, String params) {
@@ -16,6 +17,7 @@ void* getAPI(String subtype, String params) {
     if ((tmpAPI = getAPI_Ds18b20(subtype, params)) != nullptr) return tmpAPI;
     if ((tmpAPI = getAPI_Sht20(subtype, params)) != nullptr) return tmpAPI;
     if ((tmpAPI = getAPI_Dht1122(subtype, params)) != nullptr) return tmpAPI;
+    if ((tmpAPI = getAPI_Bmp280(subtype, params)) != nullptr) return tmpAPI;
     //================================================================================================================
 
     return nullptr;
---
+++ src/modules/Bmp280.cpp
@@ -0,0 +1,73 @@
+/******************************************************************
+  Used Adafruit BMP280 Driver (Barometric Pressure Sensor)
+  Support for BMP280
+  https://github.com/adafruit/Adafruit_BMP280_Library
+ ******************************************************************/
+
+
+#include "Global.h"
+#include "Classes/IoTSensor.h"
+
+#include <Adafruit_BMP280.h>
+#include <map>
+
+
+std::map<String, Adafruit_BMP280*> bmps;
+
+class Bmp280t : public IoTSensor {
+   private:
+    Adafruit_BMP280* _bmp; 
+   
+   public:
+    Bmp280t(Adafruit_BMP280* bmp, String parameters): IoTSensor(parameters) {
+        _bmp = bmp;
+    }
+    
+    void doByInterval() {
+        float value = _bmp->readTemperature();
+        if (String(value) != "nan") regEvent(value, "Bmp280t");
+            else SerialPrint("E", "Sensor DHTt", "Error");  
+    }
+
+    ~Bmp280t();
+};
+
+  
+class Bmp280p : public IoTSensor {
+   private:
+    Adafruit_BMP280* _bmp; 
+
+   public:
+    Bmp280p(Adafruit_BMP280* bmp, String parameters): IoTSensor(parameters) {
+        _bmp = bmp;
+    }
+    
+    void doByInterval() {
+        float value = _bmp->readPressure();
+        if (String(value) != "nan") {
+            value = value / 1.333224 / 100;
+            regEvent(value, "Bmp280p");
+        } else SerialPrint("E", "Sensor DHTh", "Error");  
+    }
+
+    ~Bmp280p();
+};
+
+
+void* getAPI_Bmp280(String subtype, String param) {    
+    String addr;
+    jsonRead(param, "addr", addr);
+
+    if (bmps.find(addr) == bmps.end()) {
+        bmps[addr] = new Adafruit_BMP280();
+        bmps[addr]->begin(hexStringToUint8(addr));
+    }
+
+    if (subtype == F("Bmp280t")) {
+        return new Bmp280t(bmps[addr], param);
+    } else if (subtype == F("Bmp280p")) {
+        return new Bmp280p(bmps[addr], param);
+    } else {
+        return nullptr;
+    }
+}
