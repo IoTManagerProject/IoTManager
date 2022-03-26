613d60256af43fd2a3389e2ae28d82406b788005
biver <batata@mail.ru>
2022-03-26 21:35:51 +0300

    Добавляем параметр повторения для таймера

--- src/modules/exec/Timer/Timer.cpp
+++ src/modules/exec/Timer/Timer.cpp
@@ -8,18 +8,26 @@ class Timer : public IoTItem {
    private:
     bool _unfin = false;
     bool _ticker = false;
+    bool _repeat = false;
 
    public:
     Timer(String parameters): IoTItem(parameters) {
         jsonRead(parameters, "countDown", value.valD);
         jsonRead(parameters, "ticker", _ticker);
+        jsonRead(parameters, "repeat", _repeat);
+        if (_repeat) _repeat = value.valD;  // если в параметрах просят повторить, то запоминаем настроечное значение отчета
         _unfin = !value.valD;
+
+        
     }
 
     void doByInterval() {
         if (!_unfin && value.valD) {
             value.valD--;
-            if (value.valD == 0) regEvent(value.valD, "Time's up");
+            if (value.valD == 0) {
+                regEvent(value.valD, "Time's up");
+                if (_repeat) value.valD = _repeat;
+            }
         }
 
         if (_ticker) regEvent(value.valD, "Timer tick");
