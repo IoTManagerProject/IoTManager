094bb44ed691e61b684980a813fb40f9c9f3430a
biver <batata@mail.ru>
2022-04-25 23:30:56 +0300

    Добавляем переменным возможность хранить время по шаблону

--- src/modules/system/Variable/Variable.cpp
+++ src/modules/system/Variable/Variable.cpp
@@ -1,15 +1,29 @@
 #include "Global.h"
 #include "classes/IoTItem.h"
+#include "classes/IoTRTC.h"
 
+extern IoTRTC *watch;
 
 class Variable : public IoTItem {
+   private:
+    String _dtFormat;
    public:
-    Variable(String parameters): IoTItem(parameters) { }
+    Variable(String parameters): IoTItem(parameters) {
+        jsonRead(parameters, "dtFormat", _dtFormat);
+        _dtFormat = _dtFormat;
+    }
 
     // особенность данного модуля - просто хранение значения для сценария, нет событий
     // void setValue(IoTValue Value) {
     //     value = Value;
     // }
+
+    void doByInterval() {
+        if (_dtFormat != "") {
+            value.isDecimal = false;
+            value.valS = watch->gettime(_dtFormat + " ");
+        }
+    }
 };
 
 void* getAPI_Variable(String subtype, String param) {
