a8d4e0a303b91261965bbba86e0dc77f5d82ab50
biver <batata@mail.ru>
2022-06-24 16:32:42 +0300

    Добавляем в ядро функцию сканирования шины I2C

---
+++ src/utils/I2CUtils.cpp
@@ -0,0 +1,35 @@
+#include <Wire.h>
+#include "utils/SerialPrint.h"
+
+void scanI2C() {
+    byte error, address;
+    int nDevices;
+ 
+    Serial.println("Scanning...");
+ 
+    nDevices = 0;
+    for(address = 8; address < 127; address++ ){
+        Wire.beginTransmission(address);
+        error = Wire.endTransmission();
+ 
+        if (error == 0){
+            Serial.print("I2C device found at address 0x");
+            if (address<16)
+                Serial.print("0");
+            Serial.print(address,HEX);
+            Serial.println(" !");
+ 
+            nDevices++;
+        }
+        else if (error==4) {
+            Serial.print("Unknow error at address 0x");
+            if (address<16)
+                Serial.print("0");
+            Serial.println(address,HEX);
+        } 
+    }
+    if (nDevices == 0)
+        Serial.println("No I2C devices found\n");
+    else
+        Serial.println("done\n");
+}
