a2d5a6f5e147ef1c4d3a9e5ce558afb4f4e12d39
Dmitry Borisenko <49808844+DmitryBorisenko33@users.noreply.github.com>
2022-08-31 17:32:40 +0200

    некоторые изменения

--- src/modules/sensors/Bmp280/Bmp280.cpp
+++ src/modules/sensors/Bmp280/Bmp280.cpp
@@ -4,57 +4,56 @@
   https://github.com/adafruit/Adafruit_BMP280_Library
  ******************************************************************/
 
-
 #include "Global.h"
 #include "classes/IoTItem.h"
 
 #include <Adafruit_BMP280.h>
 #include <map>
 
-
 std::map<String, Adafruit_BMP280*> bmps;
 
 class Bmp280t : public IoTItem {
    private:
-    Adafruit_BMP280* _bmp; 
-   
+    Adafruit_BMP280* _bmp;
+
    public:
-    Bmp280t(Adafruit_BMP280* bmp, String parameters): IoTItem(parameters) {
+    Bmp280t(Adafruit_BMP280* bmp, String parameters) : IoTItem(parameters) {
         _bmp = bmp;
     }
-    
+
     void doByInterval() {
         value.valD = _bmp->readTemperature();
-        if (String(value.valD) != "nan") regEvent(value.valD, "Bmp280t");
-            else SerialPrint("E", "Sensor DHTt", "Error");  
+        if (String(value.valD) != "nan")
+            regEvent(value.valD, "Bmp280t");
+        else
+            SerialPrint("E", "Sensor Bmp280t", "Error");
     }
 
-    ~Bmp280t() {};
+    ~Bmp280t(){};
 };
 
-  
 class Bmp280p : public IoTItem {
    private:
-    Adafruit_BMP280* _bmp; 
+    Adafruit_BMP280* _bmp;
 
    public:
-    Bmp280p(Adafruit_BMP280* bmp, String parameters): IoTItem(parameters) {
+    Bmp280p(Adafruit_BMP280* bmp, String parameters) : IoTItem(parameters) {
         _bmp = bmp;
     }
-    
+
     void doByInterval() {
         value.valD = _bmp->readPressure();
         if (String(value.valD) != "nan") {
             value.valD = value.valD / 1.333224 / 100;
             regEvent(value.valD, "Bmp280p");
-        } else SerialPrint("E", "Sensor DHTh", "Error");  
+        } else
+            SerialPrint("E", "Sensor Bmp280p", "Error");
     }
 
-    ~Bmp280p() {};
+    ~Bmp280p(){};
 };
 
-
-void* getAPI_Bmp280(String subtype, String param) {    
+void* getAPI_Bmp280(String subtype, String param) {
     if (subtype == F("Bmp280t") || subtype == F("Bmp280p")) {
         String addr;
         jsonRead(param, "addr", addr);
