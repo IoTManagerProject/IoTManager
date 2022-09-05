5ec718e68b8e804147f880464f6d86809d01e696
biver <batata@mail.ru>
2022-09-05 16:23:47 +0300

    Добавляем общую функцию в базу инструментов

--- src/utils/StringUtils.cpp
+++ src/utils/StringUtils.cpp
@@ -176,3 +176,20 @@ String prettyBytes(size_t size) {
     else
         return String(size / 1024.0 / 1024.0 / 1024.0) + "GB";
 }
+
+String uint64ToString(uint64_t input) {
+    String result = "";
+    uint8_t base = 10;
+
+    do {
+        char c = input % base;
+        input /= base;
+
+        if (c < 10)
+        c +='0';
+        else
+        c += 'A' - 10;
+        result = c + result;
+    } while (input);
+    return result;
+}
