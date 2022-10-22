480e20a62e01034aaca511fea68ce16ebc38b6d5
biver <batata@mail.ru>
2022-10-22 22:50:40 +0300

    Добавляем генерацию события на ошибку внутри функции печати сообщений

--- src/utils/SerialPrint.cpp
+++ src/utils/SerialPrint.cpp
@@ -1,13 +1,22 @@
 
 #include "utils/SerialPrint.h"
 
-void SerialPrint(String errorLevel, String module, String msg) {
-    String tosend;
-
-    tosend = prettyMillis(millis());
-
+void SerialPrint(String errorLevel, String module, String msg, String itemId) {
+    String tosend = prettyMillis(millis());
     tosend = tosend + " [" + errorLevel + "] [" + module + "] " + msg;
     Serial.println(tosend);
+
+    if (errorLevel == "E") {
+        msg = cleanString(msg);
+        // создаем событие об ошибке для возможной реакции в сценарии
+        if (itemId != "") {
+            IoTItems.push_back((IoTItem *)new externalVariable("{\"id\":\"" + itemId + "_onError\",\"val\":\"" + msg + "\",\"int\":1}"));
+            generateEvent(itemId + "_onError", "1");
+        } else {
+            IoTItems.push_back((IoTItem *)new externalVariable("{\"id\":\"onError\",\"val\":\"" + module + " " + msg + "\",\"int\":1}"));
+            generateEvent("onError", "1");
+        }
+    } 
 
     if (isNetworkActive()) {
         if (jsonReadInt(settingsFlashJson, F("log")) != 0) {
