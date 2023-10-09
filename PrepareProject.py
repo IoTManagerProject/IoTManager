d4413fbe3126c2e830b1c9ed71a1d3d98cf74908
IoT Manager <67171972+IoTManagerProject@users.noreply.github.com>
2023-10-10 00:56:59 +0200

    иправление бага online flasher и ota

--- PrepareProject.py
+++ PrepareProject.py
@@ -209,14 +209,7 @@ with open("platformio.ini", 'w') as configFile:
     
 # сохраняем часть применяемого профиля в папку data_svelte для загрузки на контроллер и дальнейшего переиспользования
 print(f"Saving profile {profile} in /data_svelte/flashProfile.json")
-shortProfJson = json.loads('{}')
-shortProfJson['iotmSettings'] = profJson['iotmSettings']
-shortProfJson['projectProp'] = {
-        'platformio': {
-            'default_envs': deviceName
-        }
-    }
-shortProfJson['modules'] = profJson['modules']
+shortProfJson = profJson
 with open("data_svelte/flashProfile.json", "w", encoding='utf-8') as write_file:
     json.dump(shortProfJson, write_file, ensure_ascii=False, indent=4, sort_keys=False)
     
